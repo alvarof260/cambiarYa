bca044bfe29f2b0cc232bc0fff03fd3e9df9f582
alvarof260 <alvarof260@gmail.com>
2024-10-31 14:38:42 -0300

    styles: header con estilos y la etiquetas

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function toggleNavbar() {
+    const navbar = document.querySelector(".navbar");
+    const toggleBtn = document.querySelector(".toggle-btn");
+    
+    navbar.classList.toggle("active");
+    toggleBtn.classList.toggle("active");
+  
+    // Cambiar el icono del botón
+    if (navbar.classList.contains("active")) {
+      toggleBtn.querySelector(".open").style.display = "none"; // Ocultar el ícono abierto
+      toggleBtn.querySelector(".close").style.display = "inline"; // Mostrar el ícono cerrado
+    } else {
+      toggleBtn.querySelector(".open").style.display = "inline"; // Mostrar el ícono abierto
+      toggleBtn.querySelector(".close").style.display = "none"; // Ocultar el ícono cerrado
+    }
+  }
